475e7555a7fe5851c965b964b5ec8139927c8b80
Nkidi John Nkogatse <nkiditoday@gmail.com>
2023-05-19 01:25:24 +0200

    Created the new layout for the app, still in progress though

---
+++ resources/views/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+        <div class="container mx-auto px-4">
+             <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Popular Games</h2>
+            <div class="popular-games text-sm grid grid-cols-6 gap-12 border-gray-800">
+               <div class="game mt-8">
+                   Games goes here
+               </div>
+                <div class="game mt-8">
+                    Games goes here
+                </div>
+                <div class="game mt-8">
+                    Games goes here
+                </div>
+                <div class="game mt-8">
+                    Games goes here
+                </div>
+                <div class="game mt-8">
+                    Games goes here
+                </div>
+                <div class="game mt-8">
+                    Games goes here
+                </div>
+                <div class="game mt-8">
+                    Games goes here
+                </div>
+                <div class="game mt-8">
+                    Games goes here
+                </div>
+                <div class="game mt-8">
+                    Games goes here
+                </div>
+                <div class="game mt-8">
+                    Games goes here
+                </div>
+                <div class="game mt-8">
+                    Games goes here
+                </div>
+                <div class="game mt-8">
+                    Games goes here
+                </div>
+
+            </div>
+        </div>
+@endsection
